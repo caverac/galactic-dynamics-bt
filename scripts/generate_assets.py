e3c7494d30ba13ea3c4a2a71a2b7fdc8049cff0a
carlos <caverac@gmail.com>
2025-10-30 14:12:02 -0500

    fix: create folder if not exits

--- scripts/generate_assets.py
+++ scripts/generate_assets.py
@@ -1,6 +1,10 @@
 """Generate asset files for documentation and testing."""
 
+import os
 from galactic_dynamics_bt.chapter01.frw_model import plot_frw_models
 
 if __name__ == "__main__":
+
+    os.makedirs("docs/assets/generated", exist_ok=True)
+
     plot_frw_models()
